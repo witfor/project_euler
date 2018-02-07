98a48207f5c4363481280bdf83194775a1dce46a
witfor <wgf2102@columbia.edu>
2018-02-07 12:28:03 -0500

    limit search to (n + 1)**2 terms

--- problem_3.py
+++ problem_3.py
@@ -7,9 +7,11 @@ def largest_prime_factor(n):
         return 2
     elif n % 2 == 0:
         return max(2, largest_prime_factor(n / 2))
-    for x in range(3, int(n) + 1, 2):
+    for x in range(3, (int(n) + 1)**2, 2):
         if n % x == 0:
             return max(x, largest_prime_factor(n / x))
+    # If no factors from 1 to (n + 1)**2, then n is prime
+    return n
 
 if __name__ == "__main__":
     print(largest_prime_factor(600851475143))
