ec323a46c93ed13dea0190e927151ba162eca8a0
witfor <wgf2102@columbia.edu>
2018-02-08 08:57:41 -0500

    remove deleted files

--- problem_1.py
+++
@@ -1,7 +0,0 @@
-def multiples_3_5(n):
-    '''Returns the sum of natrual numbers below n that are multiples of 3 and 5.'''
-
-    return sum(x for x in range(n) if x % 3 == 0 or x % 5 ==0 )
-
-if __name__ == "__main__":
-    print(multiples_3_5(1000))
