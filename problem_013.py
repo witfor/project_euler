6e9cabc1059b3d2a78f007c20a89489394f071d9
witfor <wgf2102@columbia.edu>
2018-02-08 21:12:21 -0500

    one line

--- problem_013.py
+++ problem_013.py
@@ -1,3 +1,2 @@
 if __name__ == "__main__":
-    filepath = 'problem_013_data.txt'
-    print(str(sum([int(x.rstrip()) for x in open(filepath)]))[:10])
+    print(str(sum([int(x.rstrip()) for x in open('problem_013_data.txt')]))[:10])
