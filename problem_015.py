0b58cb6fe0afbc654f22f23ab5391ae39351e022
witfor <wgf2102@columbia.edu>
2018-02-10 12:09:31 -0500

    add problem 15

---
+++ problem_015.py
@@ -0,0 +1,30 @@
+def lattice_paths(n, m):
+    """Returns number of lattice paths in n * m grid.
+
+    Parameters
+    ----------
+    n: number of rows
+    m: number of columns
+
+    Outputs
+    ----------
+    Number of paths from top left corner to bottom right corner in n * m grid.
+
+    Example
+    ----------
+    > lattice_paths(2, 2)
+    6
+
+    Notes
+    ----------
+    Solution achieved with dynamic programming.
+    """
+
+    list = [1] + [0 for i in range(m)]
+    for x in range(n + 1):
+        for y in range(1, m + 1):
+            list[y] += list[y - 1]
+    return list[-1]
+
+if __name__ == "__main__":
+    print(lattice_paths(20, 20))
