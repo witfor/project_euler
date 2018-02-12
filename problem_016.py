0000ec53ef7cd716e3bf537d733d4fd7ddb34f5c
witfor <wgf2102@columbia.edu>
2018-02-11 21:19:12 -0500

    improved sumDigits

--- problem_016.py
+++ problem_016.py
@@ -1,11 +1,7 @@
 def sumDigits(number):
     """Returns the sum of a number's digits."""
 
-    string_number = str(number)
-    answer = 0
-    for digit in string_number:
-        answer += int(digit)
-    return answer
+    return sum([int(x) for x in str(number)])
 
 if __name__ == "__main__":
     print(sumDigits(2**1000))
